a5d52ff90ea569061bb46767dc1dee3c9768d62e
mahdicse12 <mahdicse12@gmail.com>
2017-01-14 14:05:28 +0600

    C#

---
+++ SingleServerQueuingSystem/SingleServerQueuingSystem.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SingleServerQueuingSystem
+{
+    partial class SingleServerQueuingSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_MIT = new System.Windows.Forms.Label();
+            this.textBox_MIT = new System.Windows.Forms.TextBox();
+            this.textBox_MST = new System.Windows.Forms.TextBox();
+            this.label_MST = new System.Windows.Forms.Label();
+            this.textBox_NOC = new System.Windows.Forms.TextBox();
+            this.label_NOC = new System.Windows.Forms.Label();
+            this.label_su = new System.Windows.Forms.Label();
+            this.label_aniq = new System.Windows.Forms.Label();
+            this.label_adiq = new System.Windows.Forms.Label();
+            this.button_Evaluate = new System.Windows.Forms.Button();
+            this.label_tse = new System.Windows.Forms.Label();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_MIT
+            // 
+            this.label_MIT.Location = new System.Drawing.Point(12, 25);
+            this.label_MIT.Name = "label_MIT";
+            this.label_MIT.Size = new System.Drawing.Size(139, 22);
+            this.label_MIT.TabIndex = 0;
+            this.label_MIT.Text = "Mean Interarrival Time :";
+            // 
+            // textBox_MIT
+            // 
+            this.textBox_MIT.Location = new System.Drawing.Point(157, 25);
+            this.textBox_MIT.Name = "textBox_MIT";
+            this.textBox_MIT.Size = new System.Drawing.Size(223, 22);
+            this.textBox_MIT.TabIndex = 1;
+            // 
+            // textBox_MST
+            // 
+            this.textBox_MST.Location = new System.Drawing.Point(157, 67);
+            this.textBox_MST.Name = "textBox_MST";
+            this.textBox_MST.Size = new System.Drawing.Size(223, 22);
+            this.textBox_MST.TabIndex = 3;
+            // 
+            // label_MST
+            // 
+            this.label_MST.Location = new System.Drawing.Point(12, 67);
+            this.label_MST.Name = "label_MST";
+            this.label_MST.Size = new System.Drawing.Size(139, 22);
+            this.label_MST.TabIndex = 2;
+            this.label_MST.Text = "Mean Service Time :";
+            // 
+            // textBox_NOC
+            // 
+            this.textBox_NOC.Location = new System.Drawing.Point(157, 110);
+            this.textBox_NOC.Name = "textBox_NOC";
+            this.textBox_NOC.Size = new System.Drawing.Size(223, 22);
+            this.textBox_NOC.TabIndex = 5;
+            // 
+            // label_NOC
+            // 
+            this.label_NOC.Location = new System.Drawing.Point(12, 110);
+            this.label_NOC.Name = "label_NOC";
+            this.label_NOC.Size = new System.Drawing.Size(139, 22);
+            this.label_NOC.TabIndex = 4;
+            this.label_NOC.Text = "Number Of Customers :";
+            // 
+            // label_su
+            // 
+            this.label_su.Location = new System.Drawing.Point(12, 284);
+            this.label_su.Name = "label_su";
+            this.label_su.Size = new System.Drawing.Size(368, 22);
+            this.label_su.TabIndex = 8;
+            // 
+            // label_aniq
+            // 
+            this.label_aniq.Location = new System.Drawing.Point(12, 241);
+            this.label_aniq.Name = "label_aniq";
+            this.label_aniq.Size = new System.Drawing.Size(368, 22);
+            this.label_aniq.TabIndex = 7;
+            // 
+            // label_adiq
+            // 
+            this.label_adiq.Location = new System.Drawing.Point(12, 199);
+            this.label_adiq.Name = "label_adiq";
+            this.label_adiq.Size = new System.Drawing.Size(368, 22);
+            this.label_adiq.TabIndex = 6;
+            // 
+            // button_Evaluate
+            // 
+            this.button_Evaluate.Location = new System.Drawing.Point(224, 149);
+            this.button_Evaluate.Name = "button_Evaluate";
+            this.button_Evaluate.Size = new System.Drawing.Size(75, 23);
+            this.button_Evaluate.TabIndex = 9;
+            this.button_Evaluate.Text = "Evaluate";
+            this.button_Evaluate.UseVisualStyleBackColor = true;
+            this.button_Evaluate.Click += new System.EventHandler(this.button_Evaluate_Click);
+            // 
+            // label_tse
+            // 
+            this.label_tse.Location = new System.Drawing.Point(12, 330);
+            this.label_tse.Name = "label_tse";
+            this.label_tse.Size = new System.Drawing.Size(368, 22);
+            this.label_tse.TabIndex = 10;
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(305, 149);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(75, 23);
+            this.button_Clear.TabIndex = 11;
+            this.button_Clear.Text = "Clear";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // SingleServerQueuingSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.label_tse);
+            this.Controls.Add(this.button_Evaluate);
+            this.Controls.Add(this.label_su);
+            this.Controls.Add(this.label_aniq);
+            this.Controls.Add(this.label_adiq);
+            this.Controls.Add(this.textBox_NOC);
+            this.Controls.Add(this.label_NOC);
+            this.Controls.Add(this.textBox_MST);
+            this.Controls.Add(this.label_MST);
+            this.Controls.Add(this.textBox_MIT);
+            this.Controls.Add(this.label_MIT);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "SingleServerQueuingSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Single Server Queuing System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_MIT;
+        private System.Windows.Forms.TextBox textBox_MIT;
+        private System.Windows.Forms.TextBox textBox_MST;
+        private System.Windows.Forms.Label label_MST;
+        private System.Windows.Forms.TextBox textBox_NOC;
+        private System.Windows.Forms.Label label_NOC;
+        private System.Windows.Forms.Label label_su;
+        private System.Windows.Forms.Label label_aniq;
+        private System.Windows.Forms.Label label_adiq;
+        private System.Windows.Forms.Button button_Evaluate;
+        private System.Windows.Forms.Label label_tse;
+        private System.Windows.Forms.Button button_Clear;
+    }
+}
+
---
+++ SingleServerQueuingSystem/SingleServerQueuingSystem.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingleServerQueuingSystem
+{
+    public partial class SingleServerQueuingSystem : Form
+    {
+        public SingleServerQueuingSystem()
+        {
+            InitializeComponent();
+        }
+
+        const int Q_LIMIT = 100;
+        const int BUSY = 1;
+        const int IDLE = 0;
+
+        int next_event_type;
+        int num_custs_delayed;
+        int num_delays_required;
+        int num_events;
+        int num_in_q;
+        int server_status;
+        
+        double area_num_in_q;
+        double area_server_status;
+        double mean_interarrival;
+        double mean_service;
+        double sim_time;
+        double[] time_arrival = new double[Q_LIMIT+1];
+        double time_last_event;
+        double[] time_next_event = new double[3];
+        double total_of_delays;
+
+        private void initialize()
+        {
+            
+            sim_time = 0.0;
+            server_status = IDLE;
+            num_in_q = 0;
+            time_last_event = 0.0;
+            num_custs_delayed = 0;
+            total_of_delays = 0.0;
+            area_num_in_q = 0.0;
+            area_server_status = 0.0;
+
+            time_next_event[1] = sim_time + expon(mean_interarrival);
+            time_next_event[2] = 1.0e+30;
+        }
+
+        private void timing()
+        {
+            int i;
+            double min_time_next_event = 1.0e+29;
+            next_event_type = 0;
+
+            for (i = 1; i <= num_events; ++i)
+            {
+                if (time_next_event[i] < min_time_next_event)
+                {
+                    min_time_next_event = time_next_event[i];
+                    next_event_type = i;
+                }
+            }
+
+            
+            if (next_event_type == 0)
+            {
+                MessageBox.Show("Event list empty at time " + sim_time);
+                Application.Exit();
+            }
+
+            sim_time = min_time_next_event;
+        }
+
+        private void arrive()
+        {
+            
+            double delay;
+            time_next_event[1] = sim_time + expon(mean_interarrival);
+
+            if (server_status == BUSY)
+            {
+                ++num_in_q;
+
+                if (num_in_q > Q_LIMIT)
+                {
+                    MessageBox.Show("Overflow of the array time_arrival at time "+sim_time);
+                    Application.Exit();
+                }
+
+                time_arrival[num_in_q] = sim_time;
+
+            }
+
+            else
+            {
+                delay = 0.0;
+                total_of_delays += delay;
+                ++num_custs_delayed;
+                server_status = BUSY;
+                time_next_event[2] = sim_time + expon(mean_service);
+            }
+        }
+
+        private void depart()
+        {
+            int i;
+            double delay;
+
+            if (num_in_q == 0)
+            {
+                server_status = IDLE;
+                time_next_event[2] = 1.0e+30;
+            }
+            else
+            {
+                --num_in_q;
+                delay = sim_time - time_arrival[1];
+                total_of_delays += delay;
+                ++num_custs_delayed;
+                time_next_event[2] = sim_time + expon(mean_service);
+
+                for (i = 1; i <= num_in_q; ++i)
+                {
+                    time_arrival[i] = time_arrival[i + 1];
+                }
+            }
+        }
+
+        private void report()
+        {
+            label_adiq.Text =("Average delay in queue  " + total_of_delays / num_custs_delayed+"  minutes");
+            label_aniq.Text =("Average number in queue  " + area_num_in_q / sim_time);
+            label_su.Text =("Server utilization  " + area_server_status / sim_time);
+            label_tse.Text = ("Time simulation ended  " +sim_time+"  minutes");
+        }
+
+        private void update_time_avg_stats()
+        {
+            double time_since_last_event;
+            time_since_last_event = sim_time - time_last_event;
+            time_last_event = sim_time;
+            area_num_in_q += num_in_q * time_since_last_event;
+            area_server_status += server_status * time_since_last_event;
+        }
+
+        Random random = new Random();
+        private double expon(double mean)
+        {
+            return -mean * Math.Log(random.NextDouble());
+        }
+
+        private void button_Evaluate_Click(object sender, EventArgs e)
+        {
+            
+            num_events = 2;
+            mean_interarrival = Convert.ToDouble(textBox_MIT.Text);
+            mean_service = Convert.ToDouble(textBox_MST.Text);
+            num_delays_required = Convert.ToInt16(textBox_NOC.Text);
+
+            initialize();
+
+            while (num_custs_delayed < num_delays_required)
+            {
+                timing();
+                update_time_avg_stats();
+
+                switch (next_event_type)
+                {
+                    case 1:
+                        arrive();
+                        break;
+                    case 2:
+                        depart();
+                        break;
+                }
+            }
+            report();
+        }
+            
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            textBox_MIT.ResetText();
+            textBox_MST.ResetText();
+            textBox_NOC.ResetText();
+            label_adiq.ResetText();
+            label_aniq.ResetText();
+            label_su.ResetText();
+            label_tse.ResetText();
+        }
+    }
+}
